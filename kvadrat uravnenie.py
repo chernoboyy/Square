287602dcc400f99135e40cff566818121210d96d
Ilia <you@example.com>
2023-08-25 13:23:18 +0400

    manage ZeroDivision

--- kvadrat uravnenie.py
+++ kvadrat uravnenie.py
@@ -3,12 +3,16 @@ a = float(input())
 b = float(input())
 c = float(input())
 d = b ** 2 - 4 * a * c
-if d < 0:
-    print("Нет корней")
-elif d == 0:
-    print(-b / (2 * a))
+if a != 0:
+    if d < 0:
+        print("Нет корней")
+    elif d == 0:
+        print(-b / (2 * a))
+    else:
+        x1 = (-b + sqrt(d)) / (2 * a)
+        x2 = (-b - sqrt(d)) / (2 * a)
+        print(min(x1, x2))
+        print(max(x1, x2))
 else:
-    x1 = (-b + sqrt(d)) / (2 * a)
-    x2 = (-b - sqrt(d)) / (2 * a)
-    print(min(x1, x2))
-    print(max(x1, x2))
+    x1 = -c / b
+    print(x1)
