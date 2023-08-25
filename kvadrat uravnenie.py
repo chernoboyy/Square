7014c1645e5decea5357a47a0adc9c5932dfd0ab
Ilia <you@example.com>
2023-08-25 12:56:45 +0400

    first commit

---
+++ kvadrat uravnenie.py
@@ -0,0 +1,14 @@
+from math import sqrt
+a = float(input())
+b = float(input())
+c = float(input())
+d = b ** 2 - 4 * a * c
+if d < 0:
+    print("Нет корней")
+elif d == 0:
+    print(-b / (2 * a))
+else:
+    x1 = (-b + sqrt(d)) / (2 * a)
+    x2 = (-b - sqrt(d)) / (2 * a)
+    print(min(x1, x2))
+    print(max(x1, x2))
